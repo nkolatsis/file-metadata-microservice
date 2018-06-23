e8140d90a43968b5f41e42644df3b70eec9d95ac
Nicholas Kolatsis <13288906+nkolatsis@users.noreply.github.com>
2018-06-23 20:56:56 +0200

    Routes are laid out and stubbed.

--- metadata-ms.js
+++ metadata-ms.js
@@ -3,6 +3,16 @@ const app = express();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({extended: false});
 
+app.get("/", (req, res) => {
+  return res.send("A form to submit the file.");
+});
+
+app.post("/analyse-file", urlencodedParser, (req, res) => {
+  // curl -d upfile=@curlTestFile http://localhost:5000/analyse-file
+  let file = req.body.upfile;
+  return res.send("Process the file and output the metadata.")
+});
+
 app.listen(5000, () => console.log("Microservice running on port 5000"));
 
 module.exports = app;
