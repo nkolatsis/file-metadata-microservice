e36673e37d3a8ab91a4932807261a53a9af51fc2
Nicholas Kolatsis <13288906+nkolatsis@users.noreply.github.com>
2018-06-23 22:45:23 +0200

    Processing for uploaded files working. Project complete but with rough edges.

--- metadata-ms.js
+++ metadata-ms.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
-const urlencodedParser = bodyParser.urlencoded({extended: false});
+// const bodyParser = require("body-parser");
+// const urlencodedParser = bodyParser.urlencoded({extended: false});
 const formidable = require("formidable");
 
 
 app.get("/", (req, res) => {
-  // PMT: curl http://localhost:5000
   return res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/analyse-file", urlencodedParser, (req, res) => {
-  // curl -d upfile=@curlTestFile http://localhost:5000/analyse-file
+app.post("/analyse-file", (req, res) => {
   let form = new formidable.IncomingForm();
-  form.parse(req, (err, fields, files) => {})
-  return res.send("Process the file and output the metadata.")
+  form.parse(req, (err, fields, files) => {
+    if (err) console.error(err);
+    if (files.upfile != undefined) {
+      let metadata = {
+        name: files.upfile.name,
+        type: files.upfile.type,
+        size: files.upfile.size
+      };
+      return res.json(metadata);
+    } else res.send("There was an error processing the file. Please try again.");
+  });
 });
 
 app.listen(5000, () => console.log("Microservice running on port 5000"));
